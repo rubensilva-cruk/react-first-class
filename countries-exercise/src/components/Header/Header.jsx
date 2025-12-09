babffc1b1b95c477ee65b2111d23965463317731
Ruben Silva <ruben.silva@cancer.org.uk>
2025-12-09 21:24:52 +0000

    feat: add header component

---
+++ countries-exercise/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header flex-center">
+      <img
+        src="https://cdn.brandfetch.io/idVzEB1vAY/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B"
+        alt="Logo"
+        className="header-img"
+      />
+    </div>
+  );
+}
+
+export default Header;
