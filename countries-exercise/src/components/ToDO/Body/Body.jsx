9af3653c9bd94d16d895e5bda96d529bafb84167
Ruben Silva <ruben.silva@cancer.org.uk>
2025-12-17 17:08:36 +0000

    feat: change body file path

---
+++ countries-exercise/src/components/ToDO/Body/Body.jsx
@@ -0,0 +1,13 @@
+import './Body.css';
+import Card from '../Card/Card';
+import { useState } from 'react';
+
+function Body() {
+  return (
+    <div className="body">
+      <div className="card-group"></div>
+    </div>
+  );
+}
+
+export default Body;
